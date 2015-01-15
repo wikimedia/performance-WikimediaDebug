3b13a50170df733effab882ae9b57bb4fee5f0d2
Ori Livneh <ori@wikimedia.org>
2015-01-15 00:58:50 -0800

    Restrict to Wikimedia domains, and use alarm for shut-off timer

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 /**
  * Copyright 2015 Ori Livneh <ori@wikimedia.org>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 ( the "License" );
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,18 +13,61 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var enabled = false;
+'use strict';
 
-chrome.browserAction.onClicked.addListener( function () {
-    enabled = !enabled;  // toggle.
-    var icon = 'icon_38_' + ( enabled ? 'on' : 'off' ) + '.png';
-    chrome.browserAction.setIcon( { path: icon } );
-} );
+var debug = {
 
-chrome.webRequest.onBeforeSendHeaders.addListener( function ( details ) {
-    if ( enabled ) {
-        var header = { name: 'X-Wikimedia-Debug', value: '1' };
-        details.requestHeaders.push( header );
+    // The HTTP header we inject.
+    header: { name: 'X-Wikimedia-Debug', value: '1' },
+
+    // We intercept requests to URLs matching these patterns.
+    urlPatterns: [
+        '*://*.wikibooks.org/*',
+        '*://*.wikimedia.org/*',
+        '*://*.wikinews.org/*',
+        '*://*.wikipedia.org/*',
+        '*://*.wikiquote.org/*',
+        '*://*.wikisource.org/*',
+        '*://*.wikiversity.org/*',
+        '*://*.wikivoyage.org/*',
+        '*://*.wiktionary.org/*',
+    ],
+
+    // Current state: if true, inject header; if not, do nothing.
+    enabled: false,
+
+    // Toggle state.
+    toggle: function ( state ) {
+        debug.enabled = ( state !== undefined ) ? state : !debug.enabled;
+        debug.updateIcon();
+        if ( debug.enabled ) {
+            chrome.alarms.create( 'autoOff', { delayInMinutes: 5 } );
+        }
+    },
+
+    // Dim the toolbar icon when inactive.
+    updateIcon: function () {
+        var path = debug.enabled ? 'icon_38_on.png' : 'icon_38_off.png';
+        chrome.browserAction.setIcon( { path: path } );
+    },
+
+    // Inject header when active.
+    onBeforeSendHeaders: function ( req ) {
+        if ( debug.enabled ) {
+            req.requestHeaders.push( debug.header );
+        }
+        return { requestHeaders: req.requestHeaders };
+    },
+
+    // Automatic shutoff.
+    onAlarm: function ( alarm ) {
+        if ( alarm.name === 'autoOff' ) {
+            debug.toggle( false );
+        }
     }
-    return { requestHeaders: details.requestHeaders };
-}, { urls: [ '<all_urls>' ] }, [ 'blocking', 'requestHeaders' ] );
+};
+
+chrome.alarms.onAlarm.addListener( debug.onAlarm );
+chrome.browserAction.onClicked.addListener( debug.toggle );
+chrome.webRequest.onBeforeSendHeaders.addListener( debug.onBeforeSendHeaders,
+    { urls: debug.urlPatterns }, [ 'blocking', 'requestHeaders' ] );
