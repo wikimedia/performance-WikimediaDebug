a137d8aeac491d534b55cee8b816ac07da3e4830
Timo Tijhof <krinklemail@gmail.com>
2019-11-27 02:22:28 +0000

    popup: Minor clean up and inline docs

--- popup.js
+++ popup.js
@@ -1,33 +1,35 @@
 window.addEventListener( 'load', function () {
   'use strict';
 
-  var $options = [].slice.call( document.querySelectorAll( '.option' ) );
+  var optionElements = [].slice.call( document.querySelectorAll( '.option' ) );
 
   function onUpdate() {
       var state = { action: 'set' };
 
-      $options.forEach( function ( $el ) {
-          state[ $el.id ] = $el.checked !== undefined
-            ? $el.checked
-            : $el.value;
+      optionElements.forEach( function ( el ) {
+          state[ el.id ] = el.checked !== undefined
+            ? el.checked
+            : el.value;
       } );
 
       chrome.runtime.sendMessage( state );
   }
 
-  chrome.runtime.sendMessage( { action: 'get' }, function ( response ) {
-      $options.forEach( function ( $el ) {
-          var value = response[ $el.id ];
-
+  chrome.runtime.sendMessage(
+    { action: 'get' },
+    function ( response ) {
+      optionElements.forEach( function ( el ) {
+          var value = response[ el.id ];
           if ( typeof value === 'boolean' ) {
-              $el.checked = value;
+              el.checked = value;
           } else {
-              $el.value = value;
+              el.value = value;
           }
 
-          $el.addEventListener( 'change', onUpdate, false );
+          el.addEventListener( 'change', onUpdate );
       } );
 
+      // Remove class="hidden"
       document.body.className = '';
   } );
 } );
